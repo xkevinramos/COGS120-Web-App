95a383702bd825f52c61cd753fef48d22b136745
WaynePhung <waynewphung@aol.com>
2017-10-31 22:20:26 -0700

    Updated jQuery/javascript for hover and click events.

--- js/accordian.js
+++ js/accordian.js
@@ -1,18 +1,8 @@
 $(document).ready(function () {
-	"use strict";
-	
-	$(".buttonToList").hover(function() {
-		var toListToNum = document.getElementsByClassName("buttonToList");
-		var checkId = $(this);
-		var getId = checkId.getElementById();
-		for (var i = 0; i <= toListToNum.length; i++) {
-			if (getId === "arrow" + i) {
-				$(getId).toggleClass("toWhite");
-			}
-		}
-	});
-	
+	"use strict";	
+	var getClass = $(this).;
 	$(".buttonToList").click(function () {
 		$(".itemList").toggleClass("showList");
+		getClass.toggleClass("selected");
 	});
 });
