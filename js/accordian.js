fb0e5dce750ac5b7e710100db0eb43ae185a942e
WaynePhung <waynewphung@aol.com>
2017-10-31 23:40:15 -0700

    Latest version of family.html for A4 Assignment.

--- js/accordian.js
+++ js/accordian.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
 	"use strict";	
-	var getClass = $(this).;
 	$(".buttonToList").click(function () {
 		$(".itemList").toggleClass("showList");
-		getClass.toggleClass("selected");
 	});
 });
---
+++ js/loopList.js
@@ -0,0 +1,12 @@
+"use strict";
+var toListToNum = document.getElementsByClassName("buttonToList");
+	for (var i = 0; i < toListToNum.length; i++) {
+    	toListToNum[i].onclick = function(){
+		var list = this.nextElementSibling;
+			if (list.style.display === "none") {
+				list.style.display = "block";
+			} else {
+				list.style.display = "none";
+			}
+    	};
+	}
