5fa47d00cf154f789a63c0112cb663321e823b7b
Brendon Chen <brc019@ucsd.edu>
2017-11-05 01:28:34 -0700

    Added a dev button

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 // Gets called when the document is ready.
 $(document).ready(function() {
 
-	// Assigns functionality to the log in button.
+	// Assigns functionality to the log out button.
 	$("#log-out").click(function() {
 		console.log("Logging out!")
 		localStorage.removeItem("loggedIn");
 		window.location = "login.html"
 	});
 
+	// Assigns functionality to the localStorage.clear() button.
+	$("#clr").click(function() {
+		localStorage.clear();
+	});
 });
