2aab502c36d5ae7bc45b302fe6ac539cb35ae50f
Brendon Chen <brc019@ucsd.edu>
2017-11-05 20:51:42 -0800

    Added support for multiple users

--- js/login.js
+++ js/login.js
@@ -3,8 +3,38 @@ $(document).ready(function() {
 
 	// Assigns functionality to the log in button.
 	$("#log-in").click(function() {
-		console.log("Logging in!")
-		localStorage.setItem("loggedIn", 0);
-		window.location = "index.html"
+
+		console.log("Retrieving user data");
+
+		var user = prompt("Who are you?");
+
+		if (user != null && user != "") {
+
+			localStorage.setItem("user", user);
+
+			var localData = localStorage.getItem(user);
+
+			var cart = [];
+			var fridge = [];
+			var family = {};
+
+			if (localData) {
+				var data = JSON.parse(localData);
+				cart = data["cart"];
+				fridge = data["fridge"];
+				family = data["family"];
+			}
+
+			localStorage.setItem("cart", JSON.stringify(cart));
+			localStorage.setItem("fridge", JSON.stringify(fridge));
+			localStorage.setItem("family", JSON.stringify(family));
+
+			console.log("Logging in!")
+			localStorage.setItem("loggedIn", 0);
+			window.location = "index.html"
+		}
+		else if ( user != null) {
+			alert("Please enter something valid");
+		}
 	});
 });
