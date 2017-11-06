e72685409660ccbd263eaf78792a20fc78e1b219
Brendon Chen <brc019@ucsd.edu>
2017-11-05 21:29:44 -0800

    Removed more trailing newlines and added a comment in auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,6 @@
 console.log("Authenticating!");
 
+// If the user is not logged in, redirects them to the log in page.
 if (!localStorage.getItem("loggedIn")) {
 	window.location = "login.html"
 	console.log("Not logged in!");
