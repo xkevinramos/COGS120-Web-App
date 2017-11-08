d08efb5bd0813d141b5be437ad426d9e4dd279d9
Kevin Ramos <k2ramos@ucsd.edu>
2017-11-07 22:37:00 -0800

    Modified login

--- js/login.js
+++ js/login.js
@@ -3,23 +3,65 @@ $(document).ready(function() {
 
 	// Assigns functionality to the log in button.
 	$("#log-in").click(function() {
-
 		console.log("Retrieving user data");
 
-		var user = prompt("Who are you?");
+		// user database
+		var userdb = {}; 
+		var temp = localStorage.getItem("userdb");
 
+		if(temp) {
+			userdb = JSON.parse(temp);
+		}
+		else {
+			userdb = {};
+		}
+
+		var user = document.getElementById("username").value;
+		var pass = document.getElementById("password").value;
+
+		//var user = prompt("Who are you?");
+	
 		// Returns if user cancelled.
-		if (user == null) {
+		if(user == null && pass ==  null) {
 			return;
 		}
 
 		// Removes leading and trailing whitespaces.
 		user = user.trim();
+		pass = pass.trim();
 
 		// Returns if user did not enter any non-whitespace characters.
 		if (user === "") {
 			alert("Please enter something valid");
 			return;
+		}
+
+		// Returns if user did not enter any non-whitespace characters.
+		if (pass === "") {
+			alert("Please enter something valid");
+			return;
+		}
+
+		console.log(user);
+		console.log(pass);
+
+		if (user in userdb) {
+			if(pass === userdb[user]) {
+				console.log("Correct password");
+			}
+
+			else {
+				console.log("Wrong password");
+				alert("Incorrect password!");
+				return;
+			}
+		}
+
+		else {
+			console.log("Creating new user");
+			alert("User doesn't exist! Creating new user with password!");
+			userdb[user] = pass;
+			localStorage.setItem("userdb", JSON.stringify(userdb));
 		}
 
 		localStorage.setItem("user", user);
@@ -43,7 +85,6 @@ $(document).ready(function() {
 		localStorage.setItem("fridge", JSON.stringify(fridge));
 		localStorage.setItem("family", JSON.stringify(family));
 
-		// Logs in and redirect.
 		console.log("Logging in!")
 		localStorage.setItem("loggedIn", 0);
 		window.location = "index.html"
